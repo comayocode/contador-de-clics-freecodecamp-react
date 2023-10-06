ce6f43ae85c8d4601b460aac76215a47e71c6222
Comayocode <ronalc726@gmail.com>
2023-10-06 15:25:17 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import "./App.css";
+import freeCodeCampLogo from "./img/freecodecamp-logo.png";
+import Boton from "./components/Boton";
+import Contador from "./components/Contador";
+import { useState } from "react";
+
+function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          src={freeCodeCampLogo}
+          alt="Logo de freecodecamp"
+          className="freecodecamp-logo"
+        />
+      </div>
+
+      <div className="contenedor-principal">
+        <Contador numClics={numClics} />
+        <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
